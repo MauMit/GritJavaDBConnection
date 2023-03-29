159e3b3dffae98ce4d8814346ee2a94581a1b213
Maurice <mkaleebi@hotmail.com>
2023-03-29 13:13:32 +0200

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Applikation Startad");
+
+        //Inititiera object
+        Connection conn = null;
+        PreparedStatement ps  = null;
+        ResultSet rs = null;
+
+        while(true) {
+            System.out.println("Gör ditt menyval");
+            System.out.println("----------------");
+            System.out.println("1. Mata in data till DB");
+            System.out.println("2. Hämta data från DB");
+            System.out.println("3. Avsluta");
+        }
+
+
+    }
+}
